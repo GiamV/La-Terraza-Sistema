2d684e72d1027935be7883e9aa01848b3557c3b5
Gary2435 <garyvicencio.9@gmail.com>
2022-11-07 12:08:51 -0500

    Se agregaron los service y los dao

---
+++ LaTerrazaS/src/main/java/com/idat/laterraza/controller/rolController.java
@@ -0,0 +1,28 @@
+package com.idat.laterraza.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.idat.laterraza.entity.Rol;
+import com.idat.laterraza.service.IRolService;
+
+
+
+
+@CrossOrigin(origins= {"http://localhost/4200"})
+@RestController
+@RequestMapping("/api")
+public class rolController {
+	@Autowired
+	private IRolService rolService;
+	
+	@GetMapping("/rol")
+	public List<Rol> index(){
+		return rolService.findAll();
+	}
+}
